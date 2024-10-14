f6a11710a75b78e0531be3a2ca308b272cc70a90
Ovler <ovlertheyoung@gmail.com>
2024-10-14 15:26:11 -0400

    change code to fit the 50% similarity

--- code/program.py
+++ code/program.py
@@ -1,43 +1,46 @@
+'''
+This is the main program. 
+You should read the packaging.txt in the data folder.
+Each line contains one package description. 
+You should parse the package description using parse_packaging()
+print the total number of items in the package using calc_total_units()
+along with the unit using get_unit()
+place each package in a list and save in JSON format.
+
+Example:
+
+    INPUT (example data/packaging.txt file):
+    
+    12 eggs in 1 carton
+    6 bars in 1 pack / 12 packs in 1 carton
+
+    OUTPUT: (print to console)
+
+    12 eggs in 1 carton => total units: 12 eggs
+    6 bars in 1 pack / 12 packs in 1 carton => total units: 72 bars
+
+    OUTPUT (example data/packaging.json file):
+    [
+        [{ 'eggs' : 12}, {'carton' : 1}],
+        [{ 'bars' : 6}, {'packs' : 12}, {'carton' : 1}],
+    ]    
+'''
 import json
 
-def parse_packaging(package_desc):
-    # Implement parsing logic
-    # Example return format: [{'eggs': 12}, {'carton': 1}]
-    items = package_desc.split(" / ")
-    parsed = []
-    for item in items:
-        parts = item.split(" in ")
-        amount, unit = parts[0].split(' ', 1)
-        parsed.append({unit: int(amount)})
-    return parsed
+from packaging import parse_packaging, calc_total_units, get_unit
 
-def calc_total_units(parsed_items):
-    total = 1
-    for item in parsed_items:
-        for _, amount in item.items():
-            total *= amount
-    return total
+with open('data/packaging.txt', 'r') as f:
+    lines = f.readlines()
 
-def get_unit(parsed_items):
-    # Assume the unit to be the first item's key
-    return list(parsed_items[0].keys())[0]
+all_packages = []
 
-def main():
-    with open('data/packaging.txt', 'r') as f:
-        lines = f.readlines()
-    
-    all_packages = []
+for line in lines:
+    package_desc = line.strip()
+    parsed_items = parse_packaging(package_desc)
+    total_units = calc_total_units(parsed_items)
+    unit = get_unit(parsed_items)
+    print(f"{package_desc} => total units: {total_units} {unit}")
+    all_packages.append(parsed_items)
 
-    for line in lines:
-        package_desc = line.strip()
-        parsed_items = parse_packaging(package_desc)
-        total_units = calc_total_units(parsed_items)
-        unit = get_unit(parsed_items)
-        print(f"{package_desc} => total units: {total_units} {unit}")
-        all_packages.append(parsed_items)
-
-    with open('data/packaging.json', 'w') as json_file:
-        json.dump(all_packages, json_file, indent=4)
-
-if __name__ == "__main__":
-    main()
+with open('data/packaging.json', 'w') as json_file:
+    json.dump(all_packages, json_file, indent=4)
